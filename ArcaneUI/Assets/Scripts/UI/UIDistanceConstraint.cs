d7b32afe1c710f15bd861ffde5cef85889972f7d
Esben Jacob Sloth <esben@trueaxiongames.com>
2019-11-01 18:59:40 +0700

    Init

---
+++ ArcaneUI/Assets/Scripts/UI/UIDistanceConstraint.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIDistanceConstraint : MonoBehaviour
+{
+    [SerializeField] Transform  anchor;
+    [SerializeField] float      radius;
+    Vector3 oldPosition;
+    Vector3 d = new Vector3(0f,0f,0f);
+    void Update()
+    {
+        d += transform.position-oldPosition;
+        d -= new Vector3(0f,4f,0f);
+        float distance = Vector2.Distance(transform.position, anchor.position);
+        if(distance>radius)
+        {
+            transform.position = circleProject(transform.position, anchor.position, radius);
+        }
+        oldPosition = transform.position;
+    }
+
+    private Vector3 circleProject(Vector3 point, Vector3 anchor, float radius)
+    {
+        return (Vector3.Normalize(point - anchor) * radius) + anchor;
+    }
+}
---
+++ ArcaneUI/Assets/Scripts/UI/UIDraggable.cs
@@ -0,0 +1,26 @@
+﻿
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class UIDraggable : EventTrigger 
+{
+    private bool pointerIsDown;
+    private Vector2 oldPosition;
+    public void Update() 
+    {
+        if (pointerIsDown) 
+        {
+            Vector2 borders = GetComponentInParent<RectTransform>().sizeDelta + GetComponent<RectTransform>().sizeDelta/2f;
+            borders.x = Mathf.Abs(borders.x);
+            borders.y = Mathf.Abs(borders.y);
+            transform.position = new Vector2
+            (
+                Mathf.Clamp(Input.mousePosition.x, borders.x, Screen.width  - borders.x), 
+                Mathf.Clamp(Input.mousePosition.y, borders.y, Screen.height - borders.y)
+            );
+        }
+    }
+    public override void OnPointerDown( PointerEventData eventData ) => pointerIsDown = true;
+    public override void OnPointerUp(   PointerEventData eventData ) => pointerIsDown = false;
+}
